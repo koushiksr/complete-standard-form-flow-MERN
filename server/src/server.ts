e2933ebd2cf0d8ec43f384e4eee3788e15e3c8ba
k <k@kali>
2024-09-23 21:04:51 +0530

    commmiting all old changes

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,11 @@
-import app from './app';
-import config from './config/config';
-
+import app from "./app";
+import config from "./config/config";
+console.log("JWT_SECRET:", process.env.JWT_SECRET);
 const startServer = () => {
   try {
     app.listen(config.port, () => console.log(`Server running on port ${config.port}`));
   } catch (err) {
-    console.error('Failed to start the server', err);
+    console.error("Failed to start the server", err);
     process.exit(1);
   }
 };
