57a2f89bbace30bb2900a00ed97c10c07d464e43
k <k@kali>
2024-10-02 21:41:51 +0530

    commmiting all old changes

--- server/dist/server.js
+++ server/dist/server.js
@@ -5,12 +5,13 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const app_1 = __importDefault(require("./app"));
 const config_1 = __importDefault(require("./config/config"));
+console.log("JWT_SECRET:", process.env.JWT_SECRET);
 const startServer = () => {
     try {
         app_1.default.listen(config_1.default.port, () => console.log(`Server running on port ${config_1.default.port}`));
     }
     catch (err) {
-        console.error('Failed to start the server', err);
+        console.error("Failed to start the server", err);
         process.exit(1);
     }
 };
